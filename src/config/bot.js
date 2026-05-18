9c324f839a6bd67c139c858d3620fb6c1fa3e3f5
upscaleoverseas-cmyk <upscaleoverseas@gmail.com>
2026-05-18 10:18:08 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "The only Rani who can rule here ❤️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
